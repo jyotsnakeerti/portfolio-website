5ce1c8201380ca495cdc6bf054fc7878e5a043f5
jyotsnakeerti <jyotsnakeerti4@gmail.com>
2023-10-16 11:00:19 +0530

    Update Contact.jsx

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6] p-2' name="message" rows="6" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Send</button>
         </form>
     </div>
   )
