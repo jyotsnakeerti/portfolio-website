6a5d1d6e201cd1911a2c22b98e3c04e70d21082d
jyotsnakeerti <jyotsnakeerti4@gmail.com>
2023-10-26 20:10:46 +0530

    update

--- portfolio/src/data/data.js
+++ portfolio/src/data/data.js
@@ -23,8 +23,8 @@ export const data=[
         id:3,
         name:"Amazon-clone using react",
         image:amazon,
-        github:"https://github.com/jyotsnakeerti/amazon-clone",
-        live:"https://jyotsnakeerti.github.io/amazon-clone",
+        github:"https://github.com/jyotsnakeerti/amazon",
+        live:"https://jyotsnakeerti.github.io/amazon/",
     },
     {
         id:4,
